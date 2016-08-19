1ce084655658ac3a5fc188f2b17c04842930c04b
Ronan Frawley <rf@Ronans-MacBook-Pro.local>
2016-08-19 22:12:03 +0100

    add item model and controller

---
+++ app/controllers/item_controller.rb
@@ -0,0 +1,13 @@
+class ItemController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'item/index'
+
+  get 'item/show'
+
+  get 'item/new'
+
+  get 'item/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
