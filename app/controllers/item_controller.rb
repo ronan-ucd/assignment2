c8ca502612c6b103019593baf655769947ef86a5
Ronan Frawley <rf@Ronans-MacBook-Pro.local>
2016-08-19 22:25:14 +0100

    Delete

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -33,6 +33,15 @@ class ItemController < ApplicationController
     end
   end
 
+  def delete
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    else
+      render item_show_path(@item)
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:id, :name, :price)
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post 'item/new' => 'item#create'
   get 'item/edit/:id' => 'item#edit', as: 'item_edit'
   put 'item/edit/:id' => 'item#update'
+  delete 'item/delete/:id' => 'item#delete', as:'item_delete'
   get 'item/:id' => 'item#show', as: 'item_show'
 end
